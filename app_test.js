da1566c19e1e39959c970283712be330759354fd
tnu50 <smiha17@yahoo.com>
2017-05-01 23:27:05 +0200

    Update app_test.js

--- app_test.js
+++ app_test.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Server running at http://127.0.0.1:1337/');
+  res.end('Hello World Ini test Doang!!!\n');
+}).listen(5000, '107.170.58.106');
+console.log('Server running at http://107.170.58.106:5000/');
 console.log("Tomy testing app di server lokal!");
